24320651b35dc9957215a8b1ce5cf9e498019f2f
Jingze Ma <majingze0508@gmail.com>
2020-02-08 22:43:13 -0500

    Updates

---
+++ precache-manifest.f44254d11c0452a02489e447f4afbfdf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6badb9a77ba16484e8de85b4869d1b2",
+    "url": "/mosters-rolodex/index.html"
+  },
+  {
+    "revision": "b6f81f6a41d8b1cfb666",
+    "url": "/mosters-rolodex/static/css/main.0d9d8354.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/mosters-rolodex/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/mosters-rolodex/static/js/2.1f3314c8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b6f81f6a41d8b1cfb666",
+    "url": "/mosters-rolodex/static/js/main.6aec418a.chunk.js"
+  },
+  {
+    "revision": "3ffcbf40d7b6dd8089dd",
+    "url": "/mosters-rolodex/static/js/runtime-main.11a69633.js"
+  }
+]);
